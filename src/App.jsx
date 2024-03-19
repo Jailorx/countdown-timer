e82d5ce97aa614b6b9746fc90cad5bc5f373cc71
Shubham Bisht <bisht.shubham.emp@gmail.com>
2024-03-19 19:33:59 +0530

    handling input of target date and time

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import { useState } from "react";
-
 import "./App.css";
 
 function App() {
+  const [targetDateTime, setTargetDateTime] = useState("");
+
+  const handleDateTimeChange = (event) => {
+    const newDateTime = event.target.value;
+    setTargetDateTime(newDateTime);
+
+    console.log(new Date(newDateTime).getTime() - new Date().getTime());
+  };
   return (
     <>
       <h1>Countdown Timer</h1>
+      <input
+        type="datetime-local"
+        onChange={handleDateTimeChange}
+        value={targetDateTime}
+      />
+      <p>{targetDateTime}</p>
     </>
   );
 }
