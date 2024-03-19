694e2a9ced9ead72f701921d7e9e00f22310e76a
Shubham Bisht <bisht.shubham.emp@gmail.com>
2024-03-19 19:52:11 +0530

    Conditionally rendering the start and cancel timer button

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,34 @@ import "./App.css";
 
 function App() {
   const [targetDateTime, setTargetDateTime] = useState("");
+  const [isTimerStarted, setIsTimerStarted] = useState(false);
+  const [message, setMessage] = useState("");
 
   const handleDateTimeChange = (event) => {
     const newDateTime = event.target.value;
     setTargetDateTime(newDateTime);
+  };
 
-    console.log(new Date(newDateTime).getTime() - new Date().getTime());
+  const handleTimer = () => {
+    if (!targetDateTime)
+      setMessage("Please select target time before starting the timer");
+    else setIsTimerStarted(!isTimerStarted);
   };
   return (
-    <>
+    <div className="container">
       <h1>Countdown Timer</h1>
       <input
         type="datetime-local"
         onChange={handleDateTimeChange}
         value={targetDateTime}
       />
-      <p>{targetDateTime}</p>
-    </>
+      {isTimerStarted ? (
+        <button onClick={handleTimer}>Cancel Timer</button>
+      ) : (
+        <button onClick={handleTimer}>Start Timer</button>
+      )}
+      <p>{message}</p>
+    </div>
   );
 }
 
