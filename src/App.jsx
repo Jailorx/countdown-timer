b09b6f091b9ba932326b3b0ed89bd56cc00d9f5a
Shubham Bisht <bisht.shubham.emp@gmail.com>
2024-03-19 20:54:48 +0530

    builing timer logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,84 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
   const [targetDateTime, setTargetDateTime] = useState("");
+  const [timeRemaining, setTimeRemaining] = useState({
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  });
   const [isTimerStarted, setIsTimerStarted] = useState(false);
   const [message, setMessage] = useState("");
+  const [timer, setTimer] = useState(null);
+
+  useEffect(() => {
+    return () => {
+      clearInterval(timer);
+    };
+  }, [timer]);
 
   const handleDateTimeChange = (event) => {
     const newDateTime = event.target.value;
     setTargetDateTime(newDateTime);
   };
 
-  const handleTimer = () => {
-    if (!targetDateTime)
+  const startTimer = () => {
+    if (!targetDateTime) {
       setMessage("Please select target time before starting the timer");
-    else {
-      setIsTimerStarted(!isTimerStarted);
+    } else {
+      setTimer(
+        setInterval(() => {
+          setTimeRemaining(calculateRemainingTime(targetDateTime));
+        }, 1000)
+      );
+      setIsTimerStarted(true);
       setMessage("");
     }
   };
+
+  const stopTimer = () => {
+    clearInterval(timer);
+    setTimer(null);
+    setIsTimerStarted(false);
+    setTimeRemaining({
+      days: 0,
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
+    });
+  };
+
+  const handleTimer = () => {
+    if (isTimerStarted) {
+      stopTimer();
+    } else {
+      startTimer();
+    }
+  };
+
+  const calculateRemainingTime = () => {
+    const remainingTime = new Date(targetDateTime) - new Date();
+
+    if (remainingTime <= 0) {
+      stopTimer();
+      return {
+        days: 0,
+        hours: 0,
+        minutes: 0,
+        seconds: 0,
+      };
+    }
+
+    const days = Math.floor(remainingTime / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((remainingTime / (1000 * 60 * 60)) % 24);
+    const minutes = Math.floor((remainingTime / (1000 * 60)) % 60);
+    const seconds = Math.floor((remainingTime / 1000) % 60);
+
+    return { days, hours, minutes, seconds };
+  };
+
   return (
     <div className="container">
       <h1>Countdown Timer</h1>
@@ -34,20 +94,20 @@ function App() {
       )}
       <div className="timer">
         <div className="card">
-          <div className="value">10</div>
-          <div calssName="label">Days</div>
+          <div className="value">{timeRemaining.days}</div>
+          <div className="label">Days</div>
         </div>
         <div className="card">
-          <div className="value">10</div>
-          <div calssName="label">Hours</div>
+          <div className="value">{timeRemaining.hours}</div>
+          <div className="label">Hours</div>
         </div>
         <div className="card">
-          <div className="value">10</div>
-          <div calssName="label">Minutes</div>
+          <div className="value">{timeRemaining.minutes}</div>
+          <div className="label">Minutes</div>
         </div>
         <div className="card">
-          <div className="value">10</div>
-          <div calssName="label">Seconds</div>
+          <div className="value">{timeRemaining.seconds}</div>
+          <div className="label">Seconds</div>
         </div>
       </div>
       <p>{message}</p>
